77a5891a4b559875fa854025d43468a173beba03
Kevin Welsh <welshk91@gmail.com>
2014-03-17 14:38:32 -0400

    Moved example out of library and into own project

--- WizardExample/src/com/example/wizardexample/MainActivity.java
+++ WizardExample/src/com/example/wizardexample/MainActivity.java
@@ -1,25 +1,8 @@
-/*
- * Copyright 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.example.wizardexample;
 
-/**
- * This file is from pflammertsma's repo Android-WizardPager
- * All credit goes to him
- */
-
-package com.example.android.wizardpager;
+import com.example.android.wizardpager.wizard.WizardActivity;
+import com.example.android.wizardpager.wizard.model.AbstractWizardModel;
+import com.example.android.wizardpager.wizard.ui.StepPagerStrip;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -27,10 +10,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.view.ViewPager;
 import android.widget.Button;
-
-import com.example.android.wizardpager.wizard.WizardActivity;
-import com.example.android.wizardpager.wizard.model.AbstractWizardModel;
-import com.example.android.wizardpager.wizard.ui.StepPagerStrip;
 
 public class MainActivity extends WizardActivity {
 
@@ -60,7 +39,7 @@ public class MainActivity extends WizardActivity {
 
 	@Override
 	public void onSubmit() {
-		DialogFragment dg = new DialogFragment() {
+		DialogFragment dialog = new DialogFragment() {
 
 			@Override
 			public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -72,7 +51,7 @@ public class MainActivity extends WizardActivity {
 						.create();
 			}
 		};
-		dg.show(getSupportFragmentManager(), "place_order_dialog");
+		dialog.show(getSupportFragmentManager(), "place_order_dialog");
 	}
 
     @Override
--- WizardExample/src/com/example/wizardexample/SandwichWizardModel.java
+++ WizardExample/src/com/example/wizardexample/SandwichWizardModel.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.wizardpager;
+package com.example.wizardexample;
 
 import com.example.android.wizardpager.wizard.model.AbstractWizardModel;
 import com.example.android.wizardpager.wizard.model.BranchPage;
